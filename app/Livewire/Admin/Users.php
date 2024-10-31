33f5e8e338e1000106a1ac2076b6daf30b3fb2f2
loyd-developer <loyd.developer@gmail.com>
2024-10-31 09:39:35 +0800

    deploy 31

--- app/Livewire/Admin/Users.php
+++ app/Livewire/Admin/Users.php
@@ -5,9 +5,13 @@ namespace App\Livewire\Admin;
 use Livewire\Component;
 use Filament\Tables\Table;
 use Livewire\Attributes\Title;
+use Filament\Support\Colors\Color;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Tables\Concerns\InteractsWithTable;
 
@@ -21,8 +25,8 @@ class Users extends Component implements HasForms, HasTable
             ->query(\App\Models\User::query())
             ->columns([
                 TextColumn::make('fullname')
-                ->label('Fullname')
-                ->state(fn($record) => "$record->fname $record->mname $record->lname")->searchable(['fname','lname','mname']),
+                    ->label('Fullname')
+                    ->state(fn($record) => "$record->fname $record->mname $record->lname")->searchable(['fname', 'lname', 'mname']),
                 TextColumn::make('username')->searchable(),
                 TextColumn::make('email')->searchable(),
                 TextColumn::make('contact')->label('Contact Number'),
@@ -34,9 +38,23 @@ class Users extends Component implements HasForms, HasTable
                 // ...
             ])
             ->actions([
-                // ...
+                EditAction::make()
+                    ->form([
+                        TextInput::make('fname')->label('First Name'),
+                        TextInput::make('mname')->label('Middle Name'),
+                        TextInput::make('lname')->label('Last Name'),
+                        TextInput::make('email'),
+                        TextInput::make('address'),
+                        TextInput::make('contact')->label('Contact Number'),
+                    ])->action(function($data,$record){
+                        $record->update($data);
+                        Notification::make()
+                        ->title('Updated successfully')
+                        ->success()
+                        ->send();
+                    })->color(Color::Green)
             ])
-           ;
+        ;
     }
 
     #[Title('Users')]
